007655708ee92d0acdad38e5935a377bf5fccf0e
berkaytikenoglu <berkaytikenoglu@gmail.com>
2024-11-24 21:39:26 +0300

    Uyku plugini bitirildi.

--- src/main/java/com/armoyu/listener.java
+++ src/main/java/com/armoyu/listener.java
@@ -1,21 +1,27 @@
 package com.armoyu;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class listener implements Listener {
+
+
     // Yatağa yatan oyuncuları tutmak için bir set
-    //private final Set<Player> playersInBed = new HashSet<>();
 
     @EventHandler
     public void onMove(PlayerMoveEvent Event) {
         Player p = Event.getPlayer();
 
-        if(p.getName().equals("pandora")){
+        if (p.getName().equals("pandora")) {
             minecraftplugin.consoleSendMessage(p.getDisplayName() + " kullanıcı kısıtlandı");
             Event.setCancelled(true);
             return;
@@ -24,11 +30,32 @@ public class listener implements Listener {
     }
 
     @EventHandler
-    public void enterBed(PlayerBedEnterEvent Event){
+    public void enterBed(PlayerBedEnterEvent Event) {
         Player p = Event.getPlayer();
 
-        p.setFoodLevel(1);
-        p.setHealth(1);
+        long time = p.getWorld().getTime(); // Dünyanın zamanını al (0-23999)
+        // Eğer gece vaktiyse (12000-23999 arası gece sayılır)
+        if (!(time >= 12000 && time <= 23999)) {
+            return;
+        }
+
+        minecraftplugin.playersInBed.add(p);
+
+
+        int currentplayermembercount = Bukkit.getOnlinePlayers().size();
+        double oran = ((double) minecraftplugin.playersInBed.size() * 100 / (double) currentplayermembercount);
+        for (Player player : minecraftplugin.playersInBed) {
+            minecraftplugin.sendActionBarMessage(player, "(%" + oran + ")");
+        }
+    }
+
+    @EventHandler
+    public void BedLeave(PlayerBedLeaveEvent Event) {
+        Player p = Event.getPlayer();
+
+        minecraftplugin.playersInBed.remove(p);
+
+
     }
 
 }
--- src/main/java/com/armoyu/minecraftplugin.java
+++ src/main/java/com/armoyu/minecraftplugin.java
@@ -1,14 +1,27 @@
 package com.armoyu;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public final class minecraftplugin extends JavaPlugin {
 
 
     static String ARMOYUTag = "§b[ARMOYU] ";
     static String ARMOYUTagColor = "§e";
+
+
+    //LİSTELER
+    static final Set<Player> playersInBed = new HashSet<>();
+    //LİSTELER
 
 
     static void consoleSendMessage(String message, Color colorType) {
@@ -30,6 +43,60 @@ public final class minecraftplugin extends JavaPlugin {
         Bukkit.getConsoleSender().sendMessage(ARMOYUTag + ARMOYUTagColor + message);
     }
 
+
+    static void sendMessage(String message) {
+        Bukkit.getServer().broadcastMessage(ARMOYUTag + ARMOYUTagColor + message);
+    }
+
+    static void sendActionBarMessage(Player player, String message) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacy(message));
+    }
+
+    static void sendActionBarToAllPlayers(String message) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            sendActionBarMessage(player, message);
+        }
+    }
+
+    public void startActionBarTask() {
+        // Her saniyede bir çalışan bir görev oluştur
+
+
+        new BukkitRunnable() {
+
+            int sayac = 5;
+
+            @Override
+            public void run() {
+                int currentplayermembercount = Bukkit.getOnlinePlayers().size();
+
+                double oran = ((double) currentplayermembercount / 2);
+
+
+                if (minecraftplugin.playersInBed.size() >= oran) {
+                    sayac--;
+
+                    sendActionBarToAllPlayers(String.valueOf(sayac));
+
+                    if (sayac <= 0) {
+                        World world = Bukkit.getWorld("world");
+                        world.setTime(1000); //Gündüz yap
+                        world.setStorm(false); // Yağmuru kapat
+                        world.setThundering(false); // Yıldırımı kapat
+                        world.setWeatherDuration(6000); // Temiz hava süresini ayarla (6000 tick = 5 dakika)
+
+                        playersInBed.clear();
+                        sayac = 5;
+                        return;
+                    }
+                    return;
+                }
+                sayac = 5;
+
+            }
+        }.runTaskTimer(this, 0, 20); // 20 tick = 1 saniye
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -38,6 +105,8 @@ public final class minecraftplugin extends JavaPlugin {
         consoleSendMessage("Yesil Renk", Color.GREEN);
         consoleSendMessage("Kirmizi Renk", Color.RED);
 
+        //SAYAÇ FINKSİYONU BAŞLAT
+        startActionBarTask();
 
         getServer().getPluginManager().registerEvents(new listener(), this);
 
